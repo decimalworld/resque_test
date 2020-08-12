fabeb99e7199b370a3a05d276b8ebecc84f8dce8
Quan <leminhquan8101998@gmail.com>
2020-08-12 12:14:01 +0700

    add worker container

--- app/jobs/test_job.rb
+++ app/jobs/test_job.rb
@@ -2,7 +2,7 @@ class TestJob
   @queue = :testing_queue
 
   def self.perform params
-    testing_queue = TestingQueue.new(params[:testing_queue])
+    testing_queue = TestingQueue.new(params)
     testing_queue.save
   end
 end
